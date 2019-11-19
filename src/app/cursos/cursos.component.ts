9ae710fdf721768775961637cce0f9b6a5c5ba98
SimoneCL <simone.langer@gmail.com>
2019-11-19 13:46:27 -0300

    Alterações Aula 7 - Injeção dependencia e serviços

--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -1,3 +1,4 @@
+import { CursosService } from './cursos.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -8,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 export class CursosComponent implements OnInit {
 
   nomePortal: string;
-  cursos: string[] = ['Java','Ext JS','Angular'];
-  constructor() { 
+  cursos: string[] ;
+  /*injeção de dependecia via construtor  */
+  constructor(private cursosService: CursosService) { 
     this.nomePortal = 'http://loiane.training';
+    
+    //var servico = new CursosService();
+    this.cursos = this.cursosService.getCursos();
+
   }
 
   ngOnInit() {
